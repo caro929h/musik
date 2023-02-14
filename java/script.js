34f7858e096007ef174edd15e82d79821cb159fc
Caroline Gottlieb <carolineamaliegottlieb@gmail.com>
2023-02-14 13:57:08 +0100

    data

---
+++ java/script.js
@@ -0,0 +1,13 @@
+fetch("https://musik-cab7.restdb.io/rest/music", {
+  method: "get",
+  headers: {
+    "x-apikey": "63eb7a00478852088da6824f",
+  },
+});
+
+    .then((e) => e.json())
+    .then(doSomething);
+
+function doSomething(data) {
+  console.log(data);
+}
