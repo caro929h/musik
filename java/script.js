348a2c909613bea62e3a8fb7c71805f3f12fd3cd
Mark <markhjort99@gmail.com>
2023-02-23 18:39:55 +0100

    Database + QoL + Logo Fix

--- java/script.js
+++ java/script.js
@@ -1,21 +1,57 @@
-fetch("https://musik-cab7.restdb.io/rest/music", {
-  method: "get",
-  headers: {
-    "x-apikey": "63eb7a00478852088da6824f",
-  },
-});
+/** @type {Element | null | undefined} */
+const template = document.querySelector("#song-template")?.content;
+const list = document.querySelector("#album-list");
 
-.then((e) => e.json())
-.then(showMusics);
+if (!template || !list) {
+  console.log("Err null checks");
+} else {
+  const toMinsAndSecs = (millis) => {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
 
-function showMusics(music) {
-  products.forEach(showMusic);
-}
+  const pipeArr = (arr) => {
+    if (!Array.isArray(arr)) {
+      console.error("Result not array!");
+      return;
+    }
 
-function showMusic(music) {
-  console.log(music);
-  //fang template
-  const template = document.querySelector("#song_template").content;
-  //lav en kopi
-  const copy = template.cloneNode(true);
+    for (const song of arr) {
+      show(song);
+    }
+  };
+
+  const show = (song) => {
+    /** @type {Element} */
+    const copy = template.cloneNode(true);
+
+    const millis = Date.parse(song["song length"]) / 60;
+
+    console.log(millis);
+
+    copy.querySelector(".album-name").textContent = song["song name"];
+
+    copy.querySelector(".album-image").src = `/galleri/${song["img"]}`;
+
+    copy.querySelector(".artist").textContent = song["artist"];
+
+    copy.querySelector(".info-release").textContent = new Date(
+      song["released"]
+    ).getUTCFullYear();
+
+    copy.querySelector(".info-length").textContent = toMinsAndSecs(millis);
+
+    list.appendChild(copy);
+  };
+
+  fetch("https://musik-cab7.restdb.io/rest/music", {
+    method: "get",
+    headers: {
+      "x-apikey": "63eb7a00478852088da6824f",
+    },
+  })
+    .then((e) => e.json())
+    .then(pipeArr)
+    .catch(console.log);
 }
